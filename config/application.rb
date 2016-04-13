a09ce60138860cc46abe3a3bf6071553de815bc4
Zora Fuchs <zora.fuchs@renuo.ch>
2016-04-13 15:02:26 +0200

    configure language in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module I18nDocsDemo
   class Application < Rails::Application
+    # add yml path
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*', '*.yml').to_s]
+    # locals to support:
+    config.i18n.available_locales = [:en,:de,:it,:fr]
+
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
